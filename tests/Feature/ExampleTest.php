13a82cbc600c7b96fb9d3dca89037a1334446286
Yuki Ishihara <kai@exciter.jp>
2022-12-27 00:56:02 +0900

    テストコードの参考を追加

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,19 +3,64 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+    protected $url;
+    protected $data;
+    protected $headers;
 
-        $response->assertStatus(200);
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->url = route('welcome');
+        $this->data = [
+            'name' => 'テスト',
+            'email' => 'test@gmail.com',
+        ];
+        $this->headers = [
+            // 'Referer' => route('')
+        ];
+    }
+
+    public function getRequest()
+    {
+        $response = $this->requestGet($this->url, $this->data);
+        $response->assertSessionHasNoErrors();
+        return $response;
+    }
+
+    /**
+     * @test
+     */
+    public function Getサンプルテスト()
+    {
+        $response = $this->getRequest();
+
+        $response
+            ->assertStatus(Response::HTTP_FOUND)
+            ->assertLocation(route('welcome'));
+    }
+
+    public function postRequest()
+    {
+        $response = $this->requestPost($this->url, $this->data);
+        $response->assertSessionHasNoErrors();
+        return $response;
+    }
+
+    /**
+     * @test
+     */
+    public function Postサンプルテスト()
+    {
+        $response = $this->postRequest();
+
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertLocation(route('welcome'));
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,37 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $headers;
+    protected $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    protected function requestGet($uri, $params = []): TestResponse
+    {
+        $query = $params ? '?' . http_build_query($params) : '';
+        return $this
+            // ->actingAs($this->user) // ログインしたユーザーとして振る舞う
+            ->get($uri . $query, $this->headers);
+    }
+
+    protected function requestPost($uri, $data): TestResponse
+    {
+        return $this
+            // ->actingAs($this->user)
+            ->post($uri, $data, $this->headers);
+    }
 }
